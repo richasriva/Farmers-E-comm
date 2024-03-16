8a92c863221d6f70694c5f43a752ebc9f9f8323e
Richa Srivastava <srivastavaricha066@gmail.com>
2024-03-16 15:54:10 +0530

    Farmers E-comm

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import Header from "./component/layout/Header/Header.js";
+import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import WebFont from "webfontloader";
+import React from "react";
+import Footer from "./component/layout/Footer/Footer.js";
+
+
+function App() {
+
+  React.useEffect(()=>{
+    WebFont.load({
+      google:{
+        families:["Roboto","Droid Sans","Chilanka"]
+      }
+    })
+  });
+
+  return <Router>
+    <Header />
+
+    <Footer />
+  </Router>
+};
+
+export default App;
